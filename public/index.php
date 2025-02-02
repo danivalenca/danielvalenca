cb85690de09f7ca92c18fab2362e59b86bd3d1ed
Daniel Valenca <64115798+danivalenca@users.noreply.github.com>
2025-02-02 14:43:33 -0500

    Created main page

---
+++ public/index.php
@@ -0,0 +1,192 @@
+<?php include('include-config.php'); ?>
+
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <?php include('include-header.php'); ?>
+
+    </head>
+
+    <body>
+
+        <!-- Navbar -->
+        <nav class="bg-light p-4">
+
+            <div class="row">
+
+                <div class="col-lg-6">
+
+                    <ul class="list-inline">
+
+                        <li class="list-inline-item">About me</li>
+
+                        <li class="list-inline-item">Skills</li>
+
+                        <li class="list-inline-item">Work experience</li>
+                        
+                        <li class="list-inline-item">Projects</li>
+                        
+                        <li class="list-inline-item">Education</li>
+
+                        <li class="list-inline-item">Contact me</li>
+
+                    </ul>
+                    
+                </div>
+
+                <div class="col-lg-6 text-end">
+
+                    <ul class="list-inline">
+
+                        <li class="list-inline-item">Contact me</li>
+
+                    </ul>
+                    
+                </div>
+                
+            </div>
+                    
+        </nav>
+        <!-- Navbar -->
+
+        <!-- Header -->
+        <header class="p-5">
+
+            <!-- Wrapper -->
+            <div class="row justify-content-center">
+
+                <div class="col-lg-8">
+
+                    <h1>About me</h1>
+
+                    <h4 class="mt-4">I'm Daniel Valenca, a Web Developer based in Toronto, Canada</h4>
+                    
+                    <p class="text mt-2">Experienced Web Developer skilled in building and optimizing web solutions, including Progressive Web Apps (PWAs), using JavaScript, Node.js (Express), Python (Django, Flask), PHP (Laravel), MySQL, HTML, CSS, AJAX, and jQuery. Dedicated to continuous learning and professional growth, I actively explore new technologies to enhance my skills and create better, more efficient, high-quality applications.</p>
+
+                </div>
+
+            </div>
+            <!-- Wrapper -->
+            
+        </header>
+        <!-- Header -->
+
+        <!-- Skills -->
+        <section id="skills" class="bg-light p-5">
+
+            <!-- Wrapper -->
+            <div class="row justify-content-center">
+
+                <div class="col-lg-8">
+
+                    <h1>Skills</h1>
+                    
+                    <h4 class="mt-4">Frontend Development</h4>
+
+                    <p class="text mt-2">Expertise in creating responsive, user-friendly interfaces and Progressive Web Apps (PWAs) using HTML, CSS, JavaScript, Bootstrap, and jQuery.</p>
+
+                    <h4 class="mt-4">Backend Development</h4>
+
+                    <p class="text mt-2">Skilled in server-side programming and API development with PHP (Laravel, WordPress), Python (Django, Flask), and Node.js (Express).</p>
+
+                    <h4 class="mt-4">Database Management</h4>
+
+                    <p class="text mt-2">Proficient in designing and optimizing relational databases with MySQL.</p>
+
+                    <h4 class="mt-4">UX Design</h4>
+
+                    <p class="text mt-2">Knowledge of user-centered design principles to create intuitive and engaging web experiences.</p>
+
+                    <h4 class="mt-4">SEO & Analytics</h4>
+
+                    <p class="text mt-2">Experienced in SEO techniques and utilizing Google Analytics to analyze and optimize website performance.</p>
+
+                    <h4 class="mt-4">Version Control & Collaboration</h4>
+
+                    <p class="text mt-2">Experienced with Git, GitHub, and team-based workflows.</p>
+
+                    <h4 class="mt-4">Full-Stack Development</h4>
+
+                    <p class="text mt-2">End-to-end development of web applications, integrating frontend and backend systems.</p>
+
+                </div>
+
+            </div>
+            <!-- Wrapper -->
+            
+        </section>
+        <!-- Skills -->
+
+        <!-- Projects -->
+        <section id="projects" class="p-5">
+
+            <!-- Wrapper -->
+            <div class="row justify-content-center">
+
+                <div class="col-lg-8">
+
+                    <h1>Projects</h1>
+                    
+                    <div class="row">
+
+                        <div class="col-lg-6">
+
+                            <h4 class="mt-4">Studhub</h4>
+
+                            <div class="card border-light mt-3">
+
+                                <div class="ratio ratio-4x3">
+                                    
+                                </div>
+
+                                <div class="p-4">
+
+                                    <p class="text mt-2">Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Ipsum labore ipsa magni fuga quibusdam neque deserunt quam.</p>
+
+                                    <p class="mt-3"><a href="#" class="btn btn-dark w-100">Learn more</a></p>
+                                    
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+
+                        <div class="col-lg-6">
+
+                            <h4 class="mt-4">Studhub</h4>
+
+                            <div class="card border-light mt-3">
+
+                                <div class="ratio ratio-4x3">
+                                    
+                                </div>
+
+                                <div class="p-4">
+
+                                    <p class="text mt-2">Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Ipsum labore ipsa magni fuga quibusdam neque deserunt quam.</p>
+
+                                    <p class="mt-3"><a href="#" class="btn btn-dark w-100">Learn more</a></p>
+                                    
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- Wrapper -->
+            
+        </section>
+        <!-- Projects -->
+            
+        <?php include('include-footer.php'); ?>
+
+    </body>
+
+</html>
