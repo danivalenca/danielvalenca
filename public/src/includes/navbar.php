1071ae67a036674d0d60d7aca92590fa21112535
Daniel Valenca <64115798+danivalenca@users.noreply.github.com>
2025-09-18 14:54:36 -0400

    Add Mobile Menu

--- public/src/includes/navbar.php
+++ public/src/includes/navbar.php
@@ -15,7 +15,7 @@
                         </div>
                     </div>
 
-                    <a href="#" class="btn btn-light-primary">
+                    <a href="#offcanvas-menu" class="btn btn-light-primary" data-bs-toggle="offcanvas">
                         <i class="bi bi-list"></i>
                     </a>
 
@@ -28,3 +28,78 @@
     </div>
 
 </nav>
+
+<!-- Offcanvas Menu -->
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvas-menu" aria-labelledby="offcanvas-menu-label">
+
+    <!-- Container -->
+    <div class="d-flex flex-column vh-100">
+
+        <!-- Main Menu -->
+        <div class="flex-grow-1 overflow-auto p-4">
+
+            <div class="text-center">
+
+                <img src="https://cdn-icons-png.flaticon.com/128/1999/1999625.png" alt="Daniel Valenca" class="object-fit-contain" style="width: 86px; height: 86px;">
+                <p class="fs-5 fw-semibold mt-2">Daniel Valenca</p>
+                <p class="text-body-secondary">Web Developer</p>
+
+            </div>
+
+            <ul id="offcanvas-nav" class="nav nav-pills flex-column mt-4">
+                <li class="nav-item">
+                    <a class="nav-link" href="#intro">
+                        <i class="bi bi-person-circle me-2"></i> Intro
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#skills">
+                        <i class="bi bi-tools me-2"></i> Skills
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projects">
+                        <i class="bi bi-person-workspace me-2"></i> Projects
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#work-experience">
+                        <i class="bi bi-briefcase me-2"></i> Work Experience
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#education">
+                        <i class="bi bi-mortarboard me-2"></i> Education
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">
+                        <i class="bi bi-envelope-at me-2"></i> Contact
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+        <!-- Bottom Quick Bar -->
+        <div class="d-flex justify-content-around align-items-center p-4">
+
+            <a href="https://www.linkedin.com/in/danielvalenca/" class="btn btn-transparent-light" target="_blank" data-bs-toggle="tooltip" data-bs-title="Linkedin">
+                <i class="bi bi-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/danivalenca" class="btn btn-transparent-light" target="_blank" data-bs-toggle="tooltip" data-bs-title="Github">
+                <i class="bi bi-github"></i>
+            </a>
+
+            <a href="mailto:danielvalenca@gmail.com" class="btn btn-transparent-light" target="_blank" data-bs-toggle="tooltip" data-bs-title="Send me an email">
+                <i class="bi bi-envelope-at"></i>
+            </a>
+
+        </div>
+
+    </div>
+    <!-- Container -->
+
+</div>
+<!-- Offcanvas Menu -->
