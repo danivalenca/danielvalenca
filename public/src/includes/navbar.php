1eaef43369e244f6e214a3a5c03a18a29a851bbe
Daniel <danielvalenca@gmail.com>
2025-09-19 16:53:26 -0400

    Update Avatar

--- public/src/includes/navbar.php
+++ public/src/includes/navbar.php
@@ -9,7 +9,7 @@
                 <div class="d-flex justify-content-between align-items-center">
 
                     <div class="d-flex align-items-center">
-                        <img src="https://cdn-icons-png.flaticon.com/128/1999/1999625.png" alt="Daniel Valenca" class="object-fit-contain" style="height: 43px;">
+                        <img src="img/avatar.jpg" alt="Daniel Valenca" class="object-fit-contain" style="height: 43px;">
                         <div class="ms-3">
                             <p class="fs-5 fw-semibold">Daniel Valenca</p>
                         </div>
